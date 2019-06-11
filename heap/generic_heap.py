3d0544270e714d305673eec8061db484751a0349
Joseph <josephryan96@gmail.com>
2019-06-10 19:44:47 -0700

    BST & heap WIP

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,33 @@
 class Heap:
-  def __init__(self, comparator):
-    self.storage = []
-    self.comparator = comparator
+    def __init__(self, comparator):
+        self.storage = []
+        self.comparator = comparator
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_priority(self):
-    pass
+    def get_priority(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.storage[index] > self.storage[parent]:
+                # store = self.storage[index]
+                # self.storage[index] = self.storage[parent]
+                # self.storage[parent] = store
 
-  def _sift_down(self, index):
-    pass
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+
+                index - parent
+            else:
+                break
+
+    def _sift_down(self, index):
+        pass
