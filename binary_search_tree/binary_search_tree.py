d61dfc0d27128e898caf27c063a5c4f76bc3e642
Joseph <josephryan96@gmail.com>
2019-06-11 18:22:08 -0700

    binary_search_tree.py complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -36,4 +36,8 @@ class BinarySearchTree:
         return self.value
 
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left is not None:
+            self.left.for_each(cb)
+        if self.right is not None:
+            self.right.for_each(cb)
