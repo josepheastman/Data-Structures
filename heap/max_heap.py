f6352f62ff9582f0261daf88e1ab4f3e51334178
Joseph <josephryan96@gmail.com>
2019-06-13 19:57:51 -0700

    progress on max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,39 +1,48 @@
 class Heap:
     def __init__(self):
-        self.storage = []
+        self.storage = [0]
 
     def insert(self, value):
         self.storage.append(value)
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        if len(self.storage) > 2:
+            self.__swap(1, len(self.storage) - 1)
+            max = self.storage.pop()
+            self._sift_down(1)
+        elif len(self.storage) == 2:
+            max = self.storage.pop()
+        else:
+            max = False
+        return max
 
     def get_max(self, index):
-        maxEl = self.storage[index // 2]
-        for i in range(1 + index // 2, index):
-            maxEl = max(maxEl, self.storage[i])
-        return maxEl
+        return self.storage[0]
 
     def get_size(self):
         return len(self.storage)
+
+    def __swap(self, i, j):
+        self.storage[i], self.storage[j] = self.storage[j], self.storage[i]
 
     def _bubble_up(self, index):
         parent = index // 2
         if index <= 1:
             return
         elif self.storage[index] > self.storage[parent]:
-            self.storage(index, parent)
-            self.storage(parent)
+            self.storage[index]
+            self.storage[parent]
         self._bubble_up(parent)
 
     def _sift_down(self, index):
         left = index * 2
         right = index * 2 + 1
         largest = index
-        if len(self.heap) > left and self.heap[largest] < self.heap[left]:
+        if len(self.storage) > left and self.storage[largest] < self.storage[left]:
             largest = left
-        if len(self.heap) > right and self.heap[largest] < self.heap[right]:
+        if len(self.storage) > right and self.storage[largest] < self.storage[right]:
             largest = right
         if largest != index:
-            self.sift_down(largest)
+            self.__swap(index, largest)
+            self._sift_down(largest)
